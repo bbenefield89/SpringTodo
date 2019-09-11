ee156debb47e67e29222c7dc37ef684473761ffd
Brandon Benefield <bsquared18@gmail.com>
2019-09-11 18:54:36 -0400

    REMOVE EXTRA NEWLINE
    - src/test/java/com/github/bbenefield89/SpringTodo_API/ControllerTests/TodoControllerTest.java

--- SpringTodo_API/src/test/java/com/github/bbenefield89/SpringTodo_API/ControllerTests/TodoControllerTest.java
+++ SpringTodo_API/src/test/java/com/github/bbenefield89/SpringTodo_API/ControllerTests/TodoControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
