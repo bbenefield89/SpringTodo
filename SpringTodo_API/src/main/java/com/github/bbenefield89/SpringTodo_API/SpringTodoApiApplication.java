551fa5e7298c295cf6446989a86d05f6ed86bc66
Brandon Benefield <bsquared18@gmail.com>
2019-09-10 20:52:07 -0400

    CREATED 'SpringTodo_API' MICROSERVICE
    - Microservice in charge of the 'Todo' REST API

---
+++ SpringTodo_API/src/main/java/com/github/bbenefield89/SpringTodo_API/SpringTodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.bbenefield89.SpringTodo_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoApiApplication.class, args);
+	}
+
+}
---
+++ SpringTodo_API/src/test/java/com/github/bbenefield89/SpringTodo_API/SpringTodoApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.bbenefield89.SpringTodo_API;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTodoApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
