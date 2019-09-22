7223c18a3980deef529bd109511241ddf10276ae
Brandon Benefield <bsquared18@gmail.com>
2019-09-22 18:30:52 -0400

    BOILERPLATE AUTH0 SPA

---
+++ todoapp_ui/src/components/Profile.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useAuth0 } from "../react-auth0-wrapper";
+
+const Profile = () => {
+    const { loading, user, getIdTokenClaims, getTokenSilently } = useAuth0();
+
+    if (loading || !user) {
+        return (
+            <div>Loading...</div>
+        );
+    }
+
+    return (
+        <>
+            {test()}
+        
+            <img src={user.picture} alt="Profile" />
+
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <code>{JSON.stringify(user, null, 2)}</code>
+        </>
+    );
+
+    function test() {
+        getIdTokenClaims()
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+
+        getTokenSilently()
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+};
+
+export default Profile;
